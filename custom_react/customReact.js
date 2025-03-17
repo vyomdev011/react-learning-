0642cb4b4b1fabb01dfef2ca3908f48de2c1d100
vishal vats <vishalvats227@gmail.com>
2025-03-17 17:08:31 +0530

    add updated customReact

--- custom_react/customReact.js
+++ custom_react/customReact.js
@@ -1,10 +1,17 @@
 function customRender(reactElement,maincontainer){
     const domElement = document.createElement(reactElement.type);
     domElement.innerHTML = reactElement.children;
-    domElement.setAttribute('href',reactElement.props.href);
-    domElement.setAttribute('target',reactElement.props.target);
+
+    for(const prop in reactElement.props){
+        if(prop==='children'){
+            continue;
+        }
+        domElement.setAttribute(prop,reactElement.props[prop]);
+    }
+
     maincontainer.appendChild(domElement);
 } 
+
 const reactElement = {
     type: 'a',
     props: {
