d49ebe584029cddfc73e54b0ffd84091bc1864c9
vishal vats <vishalvats227@gmail.com>
2025-03-18 15:04:56 +0530

    add updated files of props and tailwind folder

--- 03_propsTailwind/src/App.jsx
+++ 03_propsTailwind/src/App.jsx
@@ -1,22 +1,14 @@
-import { useState,useEffect} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css';
 import Card from './components/Card.jsx';
 function App() {
 
-   // State for the counter
-   const [counter, setCounter] = useState(0);
-
-   // Increment the counter on every render (after component mounts or re-renders)
-   useEffect(() => {
-     setCounter(prevCounter => prevCounter + 1);
-   });
+ 
   return (
     <>
      <h1 className='text-3xl bg-green-500 p-3 rounded-md ' >vite with tailwind</h1>
-     <Card name="vishal" count={counter}/>
-     <Card name="vats" album = "quest" count={counter}/>
+     <Card name="vishal" count= "1" />
+     <Card name="vats" album = "quest" count= "2"/>
 
     </>
   )
